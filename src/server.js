9a122459b9aba62a6aceacadcc776e5b29297dcf
Renan Santos <renan.santos@kk.pt>
2022-03-15 16:14:43 +0000

    Created basic backend code

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
-import { GraphQLServer } from "graphql-yoga";
+const { GraphQLServer } = require("graphql-yoga");
+const path = require('path');
+const resolvers = require('./resolvers');
 
 const server = new GraphQLServer({
-
-})
+    typeDefs: path.resolve(__dirname, 'schema.graphql'),
+    resolvers
+});
 
 server.start();
